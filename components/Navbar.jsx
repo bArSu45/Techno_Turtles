fd8d2aad606912b65ec8221c76bae5f9cbf53e28
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-12-21 23:35:05 +0530

    Navbar started

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Box, Image } from "@chakra-ui/react";
+import React from "react";
 
 export default function Navbar() {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
+  return (
+    <Box background={"7895B2"} w={"100%"} h={"80px"} bg="#7895B2">
+      <Box w={"90%"} m={"auto"} border={"1px"} h={"100%"}>
+        <Image
+          h={"100%"}
+          src="https://res.cloudinary.com/dehubjbqm/image/upload/v1671645479/Logo_juatbw.png"
+        />
+      </Box>
+    </Box>
+  );
 }
