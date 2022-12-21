34a3f38c06935bdb1556921ea28becece5d99222
Barsa Rani Ojha <104006364+bArSu45@users.noreply.github.com>
2022-12-21 15:27:59 +0530

    Delete frontend directory

--- frontend/src/Pages/Payments/Payment.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Payment() {
-    return (
-        <div>
-            Payment
-        </div>
-    )
-}
--- frontend/src/Utils/backendURL.js
+++
@@ -1,3 +0,0 @@
-const backend_url = 'http://localhost:8080';
-
-module.exports = { backend_url }
