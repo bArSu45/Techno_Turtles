18311b14b04e67b0ff3d517bf2e44bc2f70f7195
Khalidshah12 <kssshah6@gmail.com>
2022-12-21 22:19:06 +0530

    structure updated

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
+import HomePage from "./homepage";
 export default function Home() {
   return (
-    <>
+    <div className="main">
       <Navbar />
+      <HomePage />
       <Footer />
-    </>
+    </div>
   );
 }
