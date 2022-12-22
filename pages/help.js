399bb088c55307526f6de7549ad18cff9e0decde
Khalidshah12 <kssshah6@gmail.com>
2022-12-22 17:52:26 +0530

    added dashboard

---
+++ pages/help.js
@@ -0,0 +1,10 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+
+export default function help() {
+    return (
+        <div>
+            <Heading>Help Page</Heading>
+        </div>
+    )
+}
